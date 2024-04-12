e7c28296b092be8cef40949da1928f45d5f9092b
Kevininininin <jinghan.wu@ufl.edu>
2024-04-12 17:15:40 -0400

    Encoder update
    New menu function and encode_password function

---
+++ experiment.py
@@ -0,0 +1,3 @@
+number = 12345
+number = str(number)
+print(number[1])
--- main program.py
+++ main program.py
@@ -1,7 +1,40 @@
-# testing push
+# Team members: Jinghan Wu, Cesar Carbaloo
+def encode_password(code):
+    encoded_password = ""
+    for char in code:
+        if int(char) <= 6:
+            encoded_char = int(char) + 3
+            encoded_char = str(encoded_char)
+            encoded_password += encoded_char
+        else:
+            encoded_char = int(char) + 3
+            encoded_char = str(encoded_char)
+            encoded_password += str(encoded_char[1])
+    # print(encoded_password)
+    return encoded_password
 
-print("hello world")
+def main():
+    while True:
+        print("Menu"
+              "\n-------------"
+              "\n1. Encode"
+              "\n2. Decode"
+              "\n3. Quit\n")
+
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            code = input("Please enter your password to encode: ")
+            encode_password(code)
+            print("Your password has been encoded and stored!\n")
+        elif option == "2":
+            pass
+        elif option == "3":
+            break
+
+
+
 
 if __name__ == '__main__':
-    pass
+    main()
 
